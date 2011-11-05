be2f4362bed5105d1db7032d6f23286bf23a751a
unknown <Feyr@weirdElf.(none)>
2011-11-05 14:12:57 -0400

    ex3 predict

--- prog3/mlclass-ex3/predict.m
+++ prog3/mlclass-ex3/predict.m
@@ -21,9 +21,17 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% add intercept to X
+X = [ ones(m,1) X ];
+a2= sigmoid(X*Theta1');
+
+% add intercept to a2
+a2 = [ ones(size(a2),1) a2 ];
+a3= sigmoid(a2 * Theta2');
 
 
-
+[v,ix]=max(a3,[],2) ;
+p=ix;
 
 
 
